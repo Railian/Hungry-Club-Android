75be0f07cdf18d916ab311defb6df22e9360ce2a
Raylyan <raylyan.e.v@gmail.com>
2015-08-05 19:20:40 +0300

    first load

---
+++ app/src/main/java/com/raylyan/hungryclub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.raylyan.hungryclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_authorization);
+    }
+}
